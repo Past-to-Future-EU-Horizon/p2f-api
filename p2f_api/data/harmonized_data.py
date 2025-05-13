6793c47e7e20e9c7d49d9df1f230cb557edcad7d
Garrett Speed <g.t.speed@uu.nl>
2025-05-13 13:21:35 +0200

    Harmonized data SQL correction and addition of data type Upon consultation with Research Engineering on database design, a data record and data type were confirmed as the right approach for this part of the database.

--- p2f_api/data/harmonized_data.py
+++ p2f_api/data/harmonized_data.py
@@ -20,12 +20,14 @@ class harmonized_int_confidence(baseSQL):
     lower_conf_interval: Mapped[float] = mapped_column(float)
     upper_conf_value: Mapped[int] = mapped_column(int)
     lower_conf_value: Mapped[int] = mapped_column(int)
-    data_record = Mapped[int] = mapped_column(BigInteger, index=True)
+    data_record: Mapped[int] = mapped_column(BigInteger, index=True)
+    data_type: Mapped[int] = mapped_column(BigInteger, index=True)
 
 class harmonized_int(baseSQL):
     pk_harm_int: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     value: Mapped[int] = mapped_column(BigInteger)
-    data_record = Mapped[int] = mapped_column(BigInteger, index=True)
+    data_record: Mapped[int] = mapped_column(BigInteger, index=True)
+    data_type: Mapped[int] = mapped_column(BigInteger, index=True)
 
 class harmonized_float_confidence(baseSQL):
     pk_harm_float_conf: Mapped[int] = mapped_column(BigInteger, primary_key=True)
@@ -34,9 +36,11 @@ class harmonized_float_confidence(baseSQL):
     lower_conf_interval: Mapped[float] = mapped_column(float)
     upper_conf_value: Mapped[float] = mapped_column(Double)
     lower_conf_value: Mapped[float] = mapped_column(Double)
-    data_record = Mapped[int] = mapped_column(BigInteger, index=True)
+    data_record: Mapped[int] = mapped_column(BigInteger, index=True)
+    data_type: Mapped[int] = mapped_column(BigInteger, index=True)
 
 class harmonized_float_confidence(baseSQL):
     pk_harm_float: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     value: Mapped[float] = mapped_column(Double)
-    data_record = Mapped[int] = mapped_column(BigInteger, index=True)
+    data_record: Mapped[int] = mapped_column(BigInteger, index=True)
+    data_type: Mapped[int] = mapped_column(BigInteger, index=True)
