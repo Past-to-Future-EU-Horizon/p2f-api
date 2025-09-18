be7710ab76aa87327a34554625f45386604a0b8f
Garrett Speed <g.t.speed@uu.nl>
2025-09-18 20:39:26 +0200

    Functions to add location metadata information

---
+++ p2f_api/service/harm_data_metadata_location.py
@@ -0,0 +1,108 @@
+from ..apilogs import logger
+from service.harm_numerical import list_numerics
+from data.db_connection import engine
+from data.harm_data_metadata import harm_location_to_record, harm_locations
+from p2f_pydantic.harm_data_metadata import harm_location as Harm_location
+from p2f_pydantic.harm_data_metadata import harm_bounding_box as Harm_bounding_box
+# Third Party Libraries
+from sqlalchemy.orm import Session
+from sqlalchemy import select, insert, delete, update
+# Batteries included libraries
+from typing import List, Optional
+from uuid import UUID
+
+def list_harm_metadata_location(
+        bounding_box: Optional[Harm_bounding_box]=None,
+        location_name: Optional[str]=None, 
+        location_code: Optional[str]=None, 
+        minimum_elevation: Optional[float]=None,
+        maximum_elevation: Optional[float]=None,
+        min_location_age: Optional[float]=None,
+        max_location_age: Optional[float]=None,
+    ) -> List[Harm_location]:
+    with Session(engine) as session:
+        stmt = select(harm_locations)
+        if bounding_box:
+            stmt = stmt.where(harm_locations.latitude <= bounding_box.north)
+            stmt = stmt.where(harm_locations.latitude >= bounding_box.south)
+            stmt = stmt.where(harm_locations.longitude <= bounding_box.east)
+            stmt = stmt.where(harm_locations.longitude >= bounding_box.west)
+        if location_name:
+            stmt = stmt.where(harm_locations.location_name == location_name)
+        if location_code:
+            stmt = stmt.where(harm_locations.location_code == location_code)
+        if minimum_elevation:
+            stmt = stmt.where(harm_locations.elevation >= minimum_elevation)
+        if maximum_elevation:
+            stmt = stmt.where(harm_locations.elevation <= maximum_elevation)
+        if min_location_age:
+            stmt = stmt.where(harm_locations.location_age >= min_location_age)
+        if max_location_age:
+            stmt = stmt.where(harm_locations.location_age <= max_location_age)
+        results = session.execute(stmt)
+    return [Harm_location(**x[0].tuple()) for x in results]
+
+def get_location(location_identifier: UUID) -> Harm_location:
+    with Session(engine) as session:
+        stmt = select(harm_locations)
+        stmt = stmt.where(harm_locations.location_identifier == location_identifier)
+        result = session.execute(stmt)
+    return Harm_location(**result[0].tuple())
+
+def create_location(new_location: Harm_location) -> Harm_location:
+    with Session(engine) as session:
+        stmt = insert(harm_locations)
+        stmt = stmt.values(**new_location)
+        execute = session.execute(stmt)
+        commit = session.commit()
+    new_pk = execute.inserted_primary_key
+    with Session(engine) as session:
+        stmt = select(harm_locations.location_identifier)
+        stmt = stmt.where(harm_locations.pk_harm_location == new_pk)
+        result = session.execute(stmt)
+    return_location = new_location
+    return_location.location_identifier = result[0]
+    return return_location
+
+def update_location(update_location: Harm_location) -> Harm_location:
+    with Session(engine) as session:
+        stmt = update(harm_locations)
+        stmt = stmt.where(update_location.location_identifier)
+        stmt = stmt.values(update_location)
+        execute = session.execute(stmt)
+        commit = session.commit()
+    return update_location
+
+def delete_location(location_identifier: UUID) -> None:
+    with Session(engine) as session:
+        stmt = delete(harm_locations)
+        stmt = stmt.where(harm_locations.location_identifier == location_identifier)
+        execute = session.execute(stmt)
+        commit = session.commit()
+    return None
+
+def assign_location_to_record(
+        location_identifier: UUID,
+        record_hash: str
+    ): 
+    with Session(engine) as session:
+        stmt = insert(harm_location_to_record)
+        stmt = stmt.values(
+            {"fk_harm_location": location_identifier,
+             "fk_data_record": record_hash}
+        )
+        execute = session.execute(stmt)
+        commit = session.commit()
+    return None
+
+def remove_location_from_record(
+        location_identifier: UUID,
+        record_hash: str
+    ): 
+    with Session(engine) as session:
+        stmt = delete(harm_location_to_record)
+        stmt = stmt.where(harm_location_to_record.fk_data_record == record_hash)
+        stmt = stmt.where(harm_location_to_record.fk_harm_location == location_identifier)
+        execute = session.execute(stmt)
+        commit = session.commit()
+    return None
---
+++ p2f_api/web/harm_data_metadata_location.py
@@ -0,0 +1,73 @@
+# Local libraries
+from ..apilogs import logger
+from ..service import harm_data_metadata_location
+from p2f_pydantic.harm_data_metadata import harm_location as Harm_location
+from p2f_pydantic.harm_data_metadata import harm_bounding_box as Harm_bounding_box
+# Third Party Libraries
+from fastapi import Body, APIRouter, Request
+# Batteries included libraries
+import uuid
+from typing import Optional, List
+
+router = APIRouter(prefix="/harm-data-locations")
+
+# List 
+@router.get("/")
+def list_harm_data_records(
+        bounding_box: Optional[Harm_bounding_box]=None,
+        location_name: Optional[str]=None, 
+        location_code: Optional[str]=None, 
+        minimum_elevation: Optional[float]=None,
+        maximum_elevation: Optional[float]=None,
+        min_location_age: Optional[float]=None,
+        max_location_age: Optional[float]=None,
+    ) -> List[Harm_location]:
+    return harm_data_metadata_location.list_harm_metadata_location(
+        bounding_box=bounding_box,
+        location_name=location_name,
+        location_code=location_code,
+        minimum_elevation=minimum_elevation,
+        maximum_elevation=maximum_elevation,
+        min_location_age=min_location_age,
+        max_location_age=max_location_age
+    )
+    
+
+# Get Single
+@router.get("/{location_identifier}")
+def get_harm_data_record(location_identifier: uuid.UUID) -> Harm_location:
+    return harm_data_metadata_location.get_location(location_identifier=location_identifier)
+
+# Create
+@router.post("/")
+def create_dataset(new_location: Harm_location) -> Harm_location:
+    return harm_data_metadata_location.create_location(new_location=new_location)
+    
+
+@router.put("/")
+def update_dataset(update_location: Harm_location) -> Harm_location:
+    return harm_data_metadata_location.update_location(update_location=update_location)
+
+
+# Delete
+@router.delete("/{location_identifier}")
+def delete_dataset(location_identifier: uuid.UUID) -> None:
+    return harm_data_metadata_location.delete_location(location_identifier=location_identifier)
+    
+@router.post("/assign")
+def assign_location_to_record(
+    location_identifier: uuid.UUID,
+    record_hash: str):
+    return harm_data_metadata_location.assign_location_to_record(
+        location_identifier=location_identifier,
+        record_hash=record_hash
+    )
+
+@router.delete("/remove")
+def remove_location_from_record(
+    location_identifier: uuid.UUID,
+    record_hash: str):
+    return harm_data_metadata_location.remove_location_from_record(
+        location_identifier=location_identifier,
+        record_hash=record_hash
+    )
