90f6f30928bfdd238c06e133592ada903e36b590
Garrett Speed <g.t.speed@uu.nl>
2025-10-23 14:14:22 +0200

    Add timeslice web component

---
+++ p2f_api/web/harm_timeslice.py
@@ -0,0 +1,41 @@
+from ..apilogs import logger
+from ..service import harm_timeslice
+from p2f_pydantic.harm_timeslices import harm_timeslice as Harm_timeslice
+# Third Party Libraries
+from fastapi import Body, APIRouter, Request
+# Batteries included libraries
+import uuid
+from typing import Optional, List
+
+router = APIRouter(prefix="/harm-data-types")
+
+router.get("/")
+def list_harm_timeslices(
+    named_time_period: Optional[str]=None, 
+    older_search_age: Optional[int]=None,
+    recent_search_age: Optional[int]=None,
+    ) -> List[Harm_timeslice]:
+    return harm_timeslice.list_harm_timeslices(
+                    named_time_period=named_time_period,
+                    older_search_age=older_search_age,
+                    recent_search_age=recent_search_age)
+
+router.get("/")
+def get_harm_timeslice(
+    timeslice_id: uuid.UUID,
+    ) -> Harm_timeslice:
+    return harm_timeslice.get_harm_timeslice(timeslice_id=timeslice_id)
+
+router.post('/')
+def create_new_timeslice(
+    new_harm_timeslice: Harm_timeslice
+    ) -> Harm_timeslice:
+    return harm_timeslice.create_new_timeslice(new_harm_timeslice=new_harm_timeslice)
+
+router.put("/")
+def update_timeslice(update_harm_timeslice: Harm_timeslice) -> Harm_timeslice:
+    return harm_timeslice.update_timeslice(update_harm_timeslice=update_harm_timeslice)
+
+router.delete("/{timeslice_id}")
+def delete_timeslice(timeslice_id: uuid.UUID) -> None:
+    return harm_timeslice.delete_timeslice(timeslice_id=timeslice_id)
