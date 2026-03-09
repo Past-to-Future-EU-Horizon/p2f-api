a152b458fc3b915795eed00dea8015c70c72167d
Garrett Speed <g.t.speed@uu.nl>
2026-03-09 14:26:28 +0100

    Add websockets as a requirement Getting unsupported upgrade requests to api

--- p2f_api/data/db_connection.py
+++ p2f_api/data/db_connection.py
@@ -25,7 +25,7 @@ from dotenv import load_dotenv
 import os
 import pathlib
 
-logger.debug(f"{fa.data} {__name__} P2F-API v0.0.23")
+logger.debug(f"{fa.data} {__name__} P2F-API v0.0.24")
 
 p = pathlib.Path(os.getcwd())
 print(p)
