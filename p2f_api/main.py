ce637dc8891780dd9c9c6d8e85ef875679461620
Garrett Speed <g.t.speed@uu.nl>
2026-04-23 13:16:30 +0200

    Fix API Initi function import in main

--- p2f_api/main.py
+++ p2f_api/main.py
@@ -15,7 +15,7 @@ from p2f_api.web import doi
 from p2f_api.web import dq_comment
 from p2f_api.web import temp_accounts
 from p2f_api.web import health
-from p2f_api.service import api_init
+from p2f_api.service import temp_accounts
 from p2f_pydantic import system as p2fsystem
 # Third Party Libraries
 from fastapi import FastAPI
@@ -25,12 +25,12 @@ import os
 app = FastAPI(
     title="Past to Future Dataset API",
     summary="APIs for the P2F team to share datasets and conform to a harmonized data model",
-    version="0.0.67",
+    version="0.0.68",
 )
 logger.debug(f"{fa.background} {__name__}")
 logger.debug("▶️  FastAPI() Started")
 
-api_init()
+temp_accounts.api_init()
 
 # For Kubernetes heartbeat/healthchecks
 app.include_router(health.router)
@@ -54,10 +54,10 @@ app.include_router(temp_accounts.router)
 def get_api_metadata() -> p2fsystem.API_Metadata:
     minimum_p2f_client_py = p2fsystem.Semantic_Version(major=0, 
                                                        minor=0, 
-                                                       patch=13)
+                                                       patch=19)
     api_version = p2fsystem.Semantic_Version(major=0, 
                                              minor=0, 
-                                             patch=67)
+                                             patch=68)
     return_class = p2fsystem.API_Metadata(pyclient_minimum_version=minimum_p2f_client_py, 
                                           api_system_version=api_version)
     return return_class
