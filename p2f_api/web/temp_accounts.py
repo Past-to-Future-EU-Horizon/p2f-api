a2e24aef9745b2c6aafb9c5449ed78ec49b8866a
Garrett Speed <g.t.speed@uu.nl>
2026-08-27 12:12:05 +0200

    Fix auth workflow to debug all statements and defer raising exceptions until the end.

--- p2f_api/web/temp_accounts.py
+++ p2f_api/web/temp_accounts.py
@@ -41,6 +41,9 @@ def request_token(request_token: Temp_Account,
              include_in_schema=False)
 def data_upload_check(request: Request, 
                       auth: Temp_Account) -> Authorization_Check:
+    """This method is intended to act as a check on the token and email address. 
+        TODO: Add a check for the location of the incoming request. This check 
+            needs to look if the request is coming from the portal. """
     result = combined_auth(request=request,
                            token=auth.token,
                            email=auth.email)
@@ -50,10 +53,15 @@ def authentication(email: str, token: str) -> bool:
     logger.debug(f"{fa.web}{fa.auth} {__name__} {stack()[0][3]}()")
     token_match = temp_accounts.evaluate_token(email=email, token=token)
     if token_match == "NotFound":
-        raise HTTPException(status_code=401, detail="Unauthorized: Token not found")
+        logger.debug("AUTHENTICATION RESULT: Token not found, raising 401 Unauthorized.")
+        return False
+        # raise HTTPException(status_code=401, detail="Unauthorized: Token not found")
     elif token_match == "Expired":
-        raise HTTPException(status_code=401, detail="Unauthorized: Token expired")
+        logger.debug("AUTHENTICATION RESULT: Token expired, raising 401 Unauthorized.")
+        return False
+        # raise HTTPException(status_code=401, detail="Unauthorized: Token expired")
     elif token_match == "Authorized":
+        logger.debug("AUTHENTICATION RESULT: Token success, returning True.")
         return True
 
 def authorization(
@@ -62,7 +70,9 @@ def authorization(
     email: Optional[str]=None,
 ) -> bool:
     logger.debug(f"{fa.web}{fa.auth} {__name__} {stack()[0][3]}()")
-    return temp_accounts.is_action_authorized(email=email, endpoint=endpoint, operation=operation)
+    result = temp_accounts.is_action_authorized(email=email, endpoint=endpoint, operation=operation)
+    logger.debug(f"AUTHORIZATION RESULT: {result}")
+    return result
 
 def combined_auth(request: Request,
                   token: str = Security(api_token), 
@@ -87,6 +97,10 @@ def combined_auth(request: Request,
                         email=email,
                         operation=operation)
         logger.debug(f"Authentication: {a1} -- Authorization: {a2}")
+        if a1 == False:
+            raise HTTPException(401, "Unauthenticated")
+        if a2 == False:
+            raise HTTPException(401, "Unauthorized")
         return a1 and a2
     
 api_token_annotation = Annotated[api_token, Security(combined_auth)]
